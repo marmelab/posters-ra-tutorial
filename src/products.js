05ca373c2db47b54759b8b768bfded782249632a
pem <pem@depinfonancy.net>
2019-08-13 14:09:34 +0200

    Initial bootstrap

---
+++ src/products.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { List, Datagrid, TextField, NumberField, UrlField, ImageField } from 'react-admin';
+
+export const ProductList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            {/*<ReferenceField source="category_id" reference="categories"><TextField source="id" /></ReferenceField>*/}
+            <TextField source="reference" />
+            <NumberField source="width" />
+            <NumberField source="height" />
+            <NumberField source="price" />
+            <UrlField source="thumbnail" />
+            <ImageField source="image" title="reference" />
+            <TextField source="description" />
+            <NumberField source="stock" />
+        </Datagrid>
+    </List>
+);
