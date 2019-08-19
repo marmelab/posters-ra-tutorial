14018e43ae3961187805da741198daa1971efa6c
pem <pem@depinfonancy.net>
2019-08-19 11:26:40 +0200

    Gildas' review

---
+++ src/categories/index.js
@@ -0,0 +1,8 @@
+import CategoryIcon from '@material-ui/icons/Bookmark';
+
+import { CategoryList } from './CategoryList';
+
+export default {
+    list: CategoryList,
+    icon: CategoryIcon,
+};
---
+++ src/products/index.js
@@ -0,0 +1,8 @@
+import ProductIcon from '@material-ui/icons/Collections';
+
+import { ProductList } from './ProductList';
+
+export default {
+    list: ProductList,
+    icon: ProductIcon,
+};
